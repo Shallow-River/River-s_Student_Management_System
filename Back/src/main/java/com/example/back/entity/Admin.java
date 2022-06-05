c04c9a75b733d47d4e7920471ba463c48e0bf59a
River Zhang <287207373@qq.com>
2022-06-05 16:48:59 +0800

    修了所属班级的BUG

--- Back/src/main/java/com/example/back/entity/Admin.java
+++ Back/src/main/java/com/example/back/entity/Admin.java
@@ -1,16 +1,16 @@
-package com.example.back.entity;
-
-import lombok.Data;
-
-import java.io.Serializable;
-
-@Data
-public class Admin implements Serializable {
-    private static final long serialVersionUID = 1L;
-
-    private String id;
-
-    private String password;
-
-    private String authority;
-}
+//package com.example.back.entity;
+//
+//import lombok.Data;
+//
+//import java.io.Serializable;
+//
+//@Data
+//public class Admin implements Serializable {
+//    private static final long serialVersionUID = 1L;
+//
+//    private String id;
+//
+//    private String password;
+//
+//    private String authority;
+//}
