48e12c66cc340e6d630b35484392def353edd3ac
Shallow-River <287207373@qq.com>
2022-06-15 18:42:28 +0800

    修改bug

--- Back/src/main/java/com/example/back/service/impl/ScoreServiceImpl.java
+++ Back/src/main/java/com/example/back/service/impl/ScoreServiceImpl.java
@@ -7,9 +7,11 @@ import com.example.back.bo.ScoreEditBo;
 import com.example.back.cache.ScoresCacheService;
 import com.example.back.common.StringConstant;
 import com.example.back.common.handle.MyException;
+import com.example.back.entity.Class;
 import com.example.back.entity.ClassReport;
 import com.example.back.entity.Scores;
 import com.example.back.entity.Student;
+import com.example.back.mapper.ClassMapper;
 import com.example.back.mapper.ScoresMapper;
 import com.example.back.mapper.StudentMapper;
 import com.example.back.service.ScoreService;
@@ -33,6 +35,9 @@ public class ScoreServiceImpl implements ScoreService {
 
     @Resource
     private ScoresCacheService scoresCacheService;
+
+    @Resource
+    private ClassMapper classMapper;
 
     @Override
     @Transactional(rollbackFor = Exception.class)
@@ -61,7 +66,7 @@ public class ScoreServiceImpl implements ScoreService {
             throw new MyException(scores, StringConstant.FAILURE_OF_SEARCH_ID);
         }
         List<ScoresVo> list = scores.stream().map(e -> {
-            Student student = studentMapper.selectById(e.getId());
+            Student student = studentMapper.selectOne(new QueryWrapper<Student>().eq("student_id", e.getStudentId()));
             ScoresVo vo = BeanUtil.toBean(e, ScoresVo.class);
             vo.setStuName(student.getStuName());
             return vo;
@@ -73,16 +78,16 @@ public class ScoreServiceImpl implements ScoreService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public List<ScoresVo> selectByName(String name) {
-        List<Scores> scores = scoresMapper.selectListByName(name);
+        List<Student> students = studentMapper.selectList(new QueryWrapper<Student>().like("stu_name", name));
 
-        if(scores.isEmpty()){
-            throw new MyException(scores, StringConstant.FAILURE_OF_SEARCH_NAME);
+        if(students.isEmpty()){
+            throw new MyException(students, StringConstant.FAILURE_OF_SEARCH_NAME);
         }
 
-        List<ScoresVo> list = scores.stream().map(e -> {
-            Student student = studentMapper.selectById(e.getId());
-            ScoresVo vo = BeanUtil.toBean(e, ScoresVo.class);
-            vo.setStuName(student.getStuName());
+        List<ScoresVo> list = students.stream().map(e -> {
+            Scores scores = scoresMapper.selectOne(new QueryWrapper<Scores>().eq("student_id", e.getStudentId()));
+            ScoresVo vo = BeanUtil.toBean(scores, ScoresVo.class);
+            vo.setStuName(e.getStuName());
             return vo;
         }).collect(Collectors.toList());
 
@@ -92,16 +97,20 @@ public class ScoreServiceImpl implements ScoreService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public List<ScoresVo> createClassReport(String className) {
-        List<Scores> scores = scoresMapper.selectListByClassName(className);
+        Class baseClass = classMapper.selectOne(new QueryWrapper<Class>().eq("class_name", className));
 
-        if(scores.isEmpty()){
-            throw new MyException(scores, StringConstant.FAILURE_OF_CREATE_REPORT);
+        Long classId = baseClass.getId();
+
+        List<Student> students = studentMapper.selectList(new QueryWrapper<Student>().eq("class_id", classId));
+
+        if(students.isEmpty()){
+            throw new MyException(students, StringConstant.FAILURE_OF_CREATE_REPORT);
         }
 
-        List<ScoresVo> list = scores.stream().map(e -> {
-            Student student = studentMapper.selectById(e.getId());
-            ScoresVo vo = BeanUtil.toBean(e, ScoresVo.class);
-            vo.setStuName(student.getStuName());
+        List<ScoresVo> list = students.stream().map(e -> {
+            Scores scores = scoresMapper.selectOne(new QueryWrapper<Scores>().eq("student_id", e.getStudentId()));
+            ScoresVo vo = BeanUtil.toBean(scores, ScoresVo.class);
+            vo.setStuName(e.getStuName());
             return vo;
         }).collect(Collectors.toList());
 
@@ -127,7 +136,7 @@ public class ScoreServiceImpl implements ScoreService {
     public List<ScoresVo> init() {
         List<Scores> scores = scoresMapper.selectList(null);
         List<ScoresVo> list = scores.stream().map(e -> {
-            Student student = studentMapper.selectById(e.getId());
+        Student student = studentMapper.selectOne(new QueryWrapper<Student>().eq("student_id", e.getStudentId()));
             ScoresVo vo = BeanUtil.toBean(e, ScoresVo.class);
             vo.setStuName(student.getStuName());
             return vo;
